18a88ce11c757560d2021c5357638a55de56548a
kiyamoon <kiyamoon@owensgroup.co.kr>
2025-06-04 16:19:47 +0900

    refactor: SRP 원칙에 따른 리팩토링

---
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -0,0 +1,14 @@
+package cleancode.minesweeper.tobe;
+
+/**
+ * 이름 변경 : shift + F6(macOS), F6 (Windows/Linux)
+ * 메서드 추출 : option + command + M (macOS), ctrl + alt + M (Windows/Linux)
+ * 상수로 추출 : option + command + C (macOS), ctrl + alt + C (Windows/Linux)
+ * try ~ catch 블록 추가 : option + command + T (macOS), ctrl + alt + T (Windows/Linux)
+ */
+public class GameApplication {   // 게임의 진입점으로 리팩토링 > 지뢰찾기 뿐만 아니라 다른 게임의 시작점으로도 사용 가능해졌다
+    public static void main(String[] args) {
+        Minesweeper minesweeper = new Minesweeper();
+        minesweeper.run();
+    }
+}
